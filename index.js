dd7533e276b08cbe3b267c37280e78634d7c4903
Daniel Kipngetich <danielkipngetich10@gmail.com>
2025-03-28 09:09:25 +0300

    updates index.js

--- index.js
+++ index.js
@@ -1,4 +1,49 @@
 /* Your Code Here */
+function createEmployeeRecord(array){
+    return {
+        firstName: array[0],
+        familyName: array[1],
+        title: array[2],
+        payPerHour: array[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+}
+function createEmployeeRecords(array){
+    return array.map(employee => createEmployeeRecord(employee))
+}
+function createTimeInEvent(dateStamp){
+    let [date, hour] = dateStamp.split(' ')
+    this.timeInEvents.push({
+        type: "TimeIn",
+        hour: parseInt(hour, 10),
+        date: date
+    })
+    return this
+}
+function createTimeOutEvent(dateStamp){
+    let [date, hour] = dateStamp.split(' ')
+    this.timeOutEvents.push({
+        type: "TimeOut",
+        hour: parseInt(hour, 10),
+        date: date
+    })
+    return this
+}
+function hoursWorkedOnDate(date){
+    let timeIn = this.timeInEvents.find(event => event.date === date)
+    let timeOut = this.timeOutEvents.find(event => event.date === date)
+    return (timeOut.hour - timeIn.hour) / 100
+}
+function wagesEarnedOnDate(date){
+    return hoursWorkedOnDate.call(this, date) * this.payPerHour
+}
+function findEmployeeByFirstName(srcArray, firstName){
+    return srcArray.find(employee => employee.firstName === firstName)
+}
+function calculatePayroll(array){
+    return array.reduce((total, employee) => total + allWagesFor.call(employee), 0)
+}
 
 /*
  We're giving you this function. Take a look at it, you might see some usage
